d23e2ec77a569818a155caaff04481c21ce585e5
ismail_abliazizov <ismail.x.abliazizov.-nd@disney.com>
2021-05-22 18:57:10 +0300

    Sout formatting refactored.

--- java-core-tutorials/src/main/java/com/java/tutorials/core/annotations/AnnotationProcessor.java
+++ java-core-tutorials/src/main/java/com/java/tutorials/core/annotations/AnnotationProcessor.java
@@ -9,10 +9,10 @@ public class AnnotationProcessor {
     }
 
     static void inspectService(Class<?> service) {
-        System.out.println(String.format("Class %s inspection..", service));
+        System.out.printf("Class %s inspection..%n", service);
         if (service.isAnnotationPresent(Service.class)) {
             Service annotation = service.getAnnotation(Service.class);
-            System.out.println(String.format("Annotation 'Service' is present: name=%s, lazyLoad=%s", annotation.name(), annotation.lazyLoad()));
+            System.out.printf("Annotation 'Service' is present: name=%s, lazyLoad=%s%n", annotation.name(), annotation.lazyLoad());
         } else {
             System.out.println("Annotation 'Service' not found.");
         }

6c6a24330c7684ebed2a6bb178e05a7a61a40c81
Ismail_Abliazizov <ismailablyazzizov@gmail.com>
2021-08-28 01:07:25 +0300

    JDBC transaction sample added.

--- java-core-tutorials/src/main/java/com/java/tutorials/core/jdbc/JdbcExample.java
+++ java-core-tutorials/src/main/java/com/java/tutorials/core/jdbc/JdbcExample.java
@@ -25,6 +25,16 @@ public class JdbcExample {
             createPeopleTable(connection);
             addSomePeople(connection);
             queryAllPeople(connection);
+
+            executeTransaction(connection);
+            queryAllPeople(connection);
+
+            executeUpdateWithCatch(connection);
+            queryAllPeople(connection);
+
+            updateRows(connection, false);
+            queryAllPeople(connection);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -60,6 +70,7 @@ public class JdbcExample {
     }
 
     private static void queryAllPeople(Connection connection) throws SQLException {
+        System.out.println("Query all people..");
         try (Statement statement = connection.createStatement()) {
             String sql = "SELECT * FROM people";
             try (ResultSet resultSet = statement.executeQuery(sql)) {
@@ -69,6 +80,42 @@ public class JdbcExample {
                     System.out.printf("name=%s, age=%s \n", name, age);
                 }
             }
+        }
+    }
+
+    private static void executeTransaction(Connection connection) throws SQLException {
+        System.out.println("Executing transaction..");
+        try {
+            // start of the transaction
+            connection.setAutoCommit(false);
+            // create and execute statements etc.
+            updateRows(connection, true);
+            // commit the changes
+            connection.commit();
+        } catch (Exception e) {
+            System.out.println("Caught exception, rolling back partial changes..");
+            connection.rollback();
+        }
+    }
+
+    private static void executeUpdateWithCatch(Connection connection) throws SQLException {
+        System.out.println("Executing transaction..");
+        try {
+            updateRows(connection, true);
+        } catch (Exception e) {
+            System.out.println("Caught exception, doing nothing..");
+        }
+    }
+
+    private static void updateRows(Connection connection, boolean throwException) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql1 = "UPDATE people SET name='John' where id=1";
+            statement.executeUpdate(sql1);
+            if (throwException) {
+                throw new IllegalStateException();
+            }
+            String sql2 = "UPDATE people SET name='Glen' where id=2";
+            statement.executeUpdate(sql2);
         }
     }
 }
